5602d34955a2a598d991e17dc819204956e7c682
ayaz-amin <ayaza2004@outlook.com>
2020-07-29 15:17:20 -0400

    github really wants me to write a summary, don't they?

--- model/model.py
+++ model/model.py
@@ -1,14 +1,14 @@
 import torch
 import torch.nn as nn
 
-from type_model import TypeModel
+from type_model import TypeModel, ConditionalTypeModel
 from rendering_model import RenderingModel
 from deformation_model import DeformationModel
 
 
 class AttentionInduction(nn.Module):
     '''
-    The full Attention Induced Program Learning model. The model
+    The full (uncoditional) Attention Induced Program Learning model. The model
     consists of three sub-models:
         a) Type model
         b) Rendering model
@@ -60,6 +60,70 @@ class AttentionInduction(nn.Module):
             The final generated image
         '''
         phw_list = self.type_model()
+        type_img = self.rendering_model(phw_list)
+        token_img = self.token_model(type_img)
+        return token_img
+
+
+class ConditionalAttentionInduction(nn.Module):
+    '''
+    The full (conditional) Attention Induced Program Learning model. The model
+    consists of three sub-models:
+        a) Type model
+        b) Rendering model
+        c) Deformation (token) model
+
+    Parameters
+    ----------
+
+    max_k: int
+        Maximum number of parts that can be present in an image at once
+    num_parts: int
+        Number of possible sub-parts that can be used to construct an image
+    filter_size: (int, int)
+        The shape of the convolution kernels for convolving over the sparse
+        representation of an image
+    image_shape: (int, int, int)
+        The shape of the (generated) image  
+    '''
+
+    def __init__(self, num_classes, max_k=5, num_parts=10, filter_size=(64, 64), image_shape=(1, 105, 105)):
+        super(ConditionalAttentionInduction, self).__init__()
+        '''
+        Attributes
+        ----------
+
+        height, width: int, int
+            Height and width of the image
+        type_model: nn.Module
+            The type model generates, well, a type
+        rendering_model: nn.Module
+            The rendering model renders the final image
+        token_model: nn.Module
+            The token model generates slight deformations onto the final image
+        '''
+
+        height, width = image_shape[1], image_shape[2]
+        self.type_model = ConditionalTypeModel(num_classes=num_classes, max_k=max_k, num_parts=num_parts, image_shape=(height, width))
+        self.rendering_model = RenderingModel(num_parts=num_parts, filter_size=filter_size, image_shape=(height, width))
+        self.token_model = DeformationModel(image_shape=image_shape)
+
+    def forward(self, class_idx):
+        '''
+        Forward generation of an image type
+
+        Parameters
+        ----------
+        class_idx: int
+            The class index
+
+        Returns
+        -------
+
+        token_image: 2D torch.tensor
+            The final generated image
+        '''
+        phw_list = self.type_model(class_idx)
         type_img = self.rendering_model(phw_list)
         token_img = self.token_model(type_img)
         return token_img
--- model/type_model.py
+++ model/type_model.py
@@ -8,7 +8,7 @@ from torch.distributions import Categorical
 
 class TypeModel(nn.Module):
     '''
-    Type model for Attention Induced Program Learning
+    Type model (uncoditional) for Attention Induced Program Learning
 
     Parameters
     ----------
@@ -119,6 +119,156 @@ class TypeModel(nn.Module):
         for i in range(k):
             part_i = self.sample_part(i)
             h_i, w_i = self.sample_coordinate(i)
+            phw_list.append((part_i, h_i, w_i))
+
+        if len(phw_list) == 0:
+            self.forward()
+
+        return np.array(phw_list)
+
+
+class ConditionalTypeModel(nn.Module):
+    '''
+    Type model (conditional) for Attention Induced Program Learning
+
+    Parameters
+    ----------
+
+    num_classes: int
+        The number of classes that can be sampled from the model
+    max_k: int
+        Maximum number of parts that can be present in an image at once
+    num_parts: int
+        Number of possible sub-parts that can be used to construct an image
+    image_shape: (int, int)
+        The shape of the (generated) image  
+    '''
+
+    def __init__(self, num_classes, max_k=5, num_parts=10, image_shape=(105, 105)):
+        super(ConditionalTypeModel, self).__init__()
+
+        '''
+        Attributes
+        ----------
+
+        k: 2D torch.tensor
+            Tensor (2D) specifying the probability of selecting part count
+        P: 3D torch.tensor
+            Tensor (3D) specifying the probability of selecting a part at part-count k
+        cd_h, cd_w: int, int
+            Height and width boundaries for restricting the sampling of coordinates
+        height_matrix: 3D torch.tensor
+            Tensor (3D) specifying the probability of selecting height index (column) at part-count k
+        width_matrix: 3D torch.tensor
+            Tensor (3D) specifying the probability of selecting width index (row) at part-count k
+        '''
+
+        self.num_classes = num_classes
+
+        self.k = nn.Parameter(torch.ones((num_classes, max_k)) / 1.0 * max_k)
+        self.P = nn.Parameter(torch.ones((num_classes, max_k, num_parts)) / 1.0 * num_parts)
+
+        cd_h, cd_w = image_shape[0] - 5, image_shape[1] - 5
+        self.height_matrix = nn.Parameter(torch.ones((num_classes, max_k, cd_h)) / 1.0 * cd_h)
+        self.width_matrix = nn.Parameter(torch.ones((num_classes, max_k, cd_w)) / 1.0 * cd_w)
+
+    def sample_k(self, class_idx):
+        '''
+        Sample number of sub-parts
+
+        Parameters
+        ----------
+
+        class_idx: int
+            The class index
+
+        Returns
+        -------
+        k: torch.tensor
+            Part-count
+        '''
+
+        k_class = self.k[class_idx]
+        dist = Categorical(k_class)
+        return dist.sample()
+    
+    def sample_part(self, class_idx, k_i):
+        '''
+        Sample sub-part at part-count k_i
+
+        Parameters
+        ----------
+        class_idx: int
+            The class index
+
+        k_i: torch.tensor
+            Ith part-count
+        
+        Returns
+        -------
+        part_i: torch.tensor
+            Ith sub-part index
+        '''
+
+        P_class = self.P[class_idx]
+        P_dist = P_class[k_i]
+        dist = Categorical(P_dist)
+        return dist.sample()
+    
+    def sample_coordinate(self, class_idx, k_i):
+        '''
+        Sample coordinates of sub-parts
+
+        Parameters
+        ----------
+        class_idx: int
+            The class index
+
+        k_i: torch.tensor
+            Ith part-count
+        
+        Returns
+        -------
+        h_i, w_i: torch.tensor, torch.tensor
+            Ith height and width
+        '''
+
+        cd_h_class = self.height_matrix[class_idx]
+        cd_w_class = self.width_matrix[class_idx]
+
+        cd_h_dist = cd_h_class[k_i]
+        cd_w_dist = cd_w_class[k_i]
+
+        dist_h = Categorical(cd_h_dist)
+        dist_w = Categorical(cd_w_dist)
+
+        return dist_h.sample(), dist_w.sample()
+
+    def forward(self, class_idx):
+        '''
+        Generate image type
+
+        Parameters
+        ----------
+        class_idx: int
+            The class index
+
+        Returns
+        -------
+        phw_list: [(torch.tensor, torch.tensor, torch.tensor)]
+            A list containing the feature index, the row, and the column of each part
+            Needed for convolving over the final image
+        '''
+
+        assert class_idx <= self.num_classes - 1, \
+            "Class index is out of bounds"
+
+        k = self.sample_k(class_idx)
+        phw_list = []
+
+        for i in range(k):
+            part_i = self.sample_part(class_idx, i)
+            h_i, w_i = self.sample_coordinate(class_idx, i)
             phw_list.append((part_i, h_i, w_i))
 
         if len(phw_list) == 0:
